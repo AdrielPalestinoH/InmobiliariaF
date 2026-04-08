b1bab24d2ff25121debbc5d0cbe5e7f516db30e0
Adriel <isc.adrielpalestino@gmail.com>
2026-04-08 14:37:15 -0600

    editar inmueble

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
-
+import { Router } from '@angular/router'; // <--- Importante
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private apiUrl = 'https://inmobiliaria-api-cvewh6fphthve7ad.westus-01.azurewebsites.net/api/v1/auth/login-request';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   verificarToken(token: string) {
   return this.http.get<any>(
@@ -22,7 +22,14 @@ export class AuthService {
   })
 }
   logout() {
+    // 1. Borramos la información de sesión
     localStorage.removeItem('usuario');
+    
+    // 2. Si manejas tokens u otros datos, bórralos también
+    localStorage.clear(); // Opción radical para borrar todo
+
+    // 3. Redireccionamos a la raíz
+    this.router.navigate(['/']);
   }
 
   getUsuarioActual() {
